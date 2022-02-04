79f322b518ad953ee477f8993c0405cdf652ebdb
DESKTOP-SO2RK1E\Neeraj <nyadav@dminc.com>
2022-02-04 22:20:32 +0530

    Custom Extension BackOffice

---
+++ practicebackoffice/gensrc/org/practicebackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 28-Jan-2022, 10:59:40 AM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.practicebackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "practicebackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
